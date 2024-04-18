3adf811e092cf5da9f191a0ec7c20c5bbdc01e7f
rounak-meyur <rm5nz@virginia.edu>
2024-04-18 12:08:33 -0700

    updated run.sh

--- run.sh
+++ run.sh
@@ -11,5 +11,6 @@ else
 	fi
 	pkill -9 helics_broker
 	pkill -9 python
+	oedisi build --component-dict scenario/$scenario/components.json --system scenario/$scenario/system.json --target-directory build_$scenario
 	oedisi run --runner build_$scenario/system_runner.json
 fi
